cc521bad6aa0ce741e0d11977d2744d61b07ff6d
Michael Sawaya <mike_gogeta@hotmail.com>
2012-04-23 13:15:24 -0700

    Added More Irems

--- RPG/AStarGame/AStarGame/Item.cs
+++ RPG/AStarGame/AStarGame/Item.cs
@@ -23,7 +23,7 @@ namespace AStarGame
     public enum ItemType {ARMOR, ROBE, CLOTHING, MELEE_WEAPON, MAGIC_WEAPON, RECOVERY_POTION, STAT_POTION, QUEST};
     public enum ItemEffectType {AGL, ATK, DEF, MAG_ATK, HP, MP}
     public enum ItemTargetType { SELF, SINGLE, ALL }
-    public enum ItemUsageType { TEMP, PERM }
+    public enum ItemUsageType { TEMP, PERM, REGEN }
 
     class ItemEffect
     {
@@ -99,7 +99,11 @@ namespace AStarGame
 
     public enum Item
     {
-        #region POTIONS
+        #region QUEST
+
+        #endregion
+
+        #region RECOVERY POTION
         [ItemAttribute(ItemType.RECOVERY_POTION, new ItemEffect[] {(new ItemEffect(ItemEffectType.HP, ItemTargetType.SINGLE, ItemUsageType.TEMP, false, 100))}, 20, "Low Health Potion", "Heals 100 HP.")] 
         HP_POTION_100,
         [ItemAttribute(ItemType.RECOVERY_POTION, new ItemEffect[] { (new ItemEffect(ItemEffectType.HP, ItemTargetType.SINGLE, ItemUsageType.TEMP, false, 250)) }, 110, "Medium Health Potion", "Heals 250 HP.")]
@@ -114,25 +118,101 @@ namespace AStarGame
         MP_POTION_500,
         #endregion
 
+        #region STAT POTION
+        #endregion
+
         #region CLOTHING
         [ItemAttribute(ItemType.CLOTHING, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 3)) }, 20, "Cotton Shirt", "Better than being naked...I guess...")]
         CLOTHING_1,
         [ItemAttribute(ItemType.CLOTHING, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 7)) }, 50, "Travel Vest", "Light, fashionable armor.")]
         CLOTHING_2,
-        [ItemAttribute(ItemType.CLOTHING, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 21)) }, 1100, "Full Metal Vest", "More metal than human!")]
+        [ItemAttribute(ItemType.CLOTHING, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 16)) }, 400, "Fur Coat", "I hope its Faux.")]
         CLOTHING_3,
-        [ItemAttribute(ItemType.CLOTHING, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 22)), (new ItemEffect(ItemEffectType.AGL, ItemTargetType.SELF, ItemUsageType.PERM, true, 15)) }, 1700, "Elven Mail", "Looks more metrosexual than asexual to me.")]
+        [ItemAttribute(ItemType.CLOTHING, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 18)), (new ItemEffect(ItemEffectType.MP, ItemTargetType.SELF, ItemUsageType.PERM, true, 8)) }, 850, "Mage's Clothes", "We stole them from magi.")]
         CLOTHING_4,
-        [ItemAttribute(ItemType.CLOTHING, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 21)) }, 1100, "Full Metal Vest", "More metal than human!")]
-        CLOTHING_3,
+        [ItemAttribute(ItemType.CLOTHING, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 21)) }, 1100, "Full Metal Vest", "More metal than human.")]
+        CLOTHING_5,
+        [ItemAttribute(ItemType.CLOTHING, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 22)), (new ItemEffect(ItemEffectType.AGL, ItemTargetType.SELF, ItemUsageType.PERM, true, 15)) }, 1700, "Elven Mail", "Seems more metrosexual than asexual to me.")]
+        CLOTHING_6,
+        [ItemAttribute(ItemType.CLOTHING, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 28)) }, 2800, "Festival Coat", "So many tassles...")]
+        CLOTHING_7,
+        [ItemAttribute(ItemType.CLOTHING, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 25)), (new ItemEffect(ItemEffectType.AGL, ItemTargetType.SELF, ItemUsageType.PERM, true, 10)) }, 2800, "Kimono", "The ultimate in Japanese only-wear.")]
+        CLOTHING_8,
+        [ItemAttribute(ItemType.CLOTHING, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 37)), (new ItemEffect(ItemEffectType.AGL, ItemTargetType.SELF, ItemUsageType.PERM, true, 40)) }, 4000, "Wild Coat", "Bring out your inner beast.")]
+        CLOTHING_9,
+        [ItemAttribute(ItemType.CLOTHING, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 42))}, 9800, "Storm Gear", "Something for that perfect storm.")]
+        CLOTHING_10,
+        [ItemAttribute(ItemType.CLOTHING, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 43)), (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 5)) }, 10400, "Erinyes Tunic", "Might make you a bit vengeful.")]
+        CLOTHING_11,
+        [ItemAttribute(ItemType.CLOTHING, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 47)) }, 14900, "Mythril Clothes", "Made from the best stuff on Earth.")]
+        CLOTHING_12,
         #endregion 
 
         #region ROBE
-        [ItemAttribute(ItemType.ROBE, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 3)) }, 20, "Cotton Shirt", "Better than being naked...I guess...")]
+        [ItemAttribute(ItemType.ROBE, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 4)) }, 25, "Cotton Robe", "You don't even need pants with them.")]
         ROBE_1,
+        [ItemAttribute(ItemType.ROBE, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 10)) }, 200, "Travel Robe", "Just enough protection to leave your house.")]
+        ROBE_2,
+        [ItemAttribute(ItemType.ROBE, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 20)) }, 1400, "Silk Robe", "The best in comfort.")]
+        ROBE_3,
+        [ItemAttribute(ItemType.ROBE, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 26)) }, 2400, "Jerkin", "Let's just say you will have a hard time putting them on.")]
+        ROBE_4,
+        [ItemAttribute(ItemType.ROBE, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 29)), (new ItemEffect(ItemEffectType.MP, ItemTargetType.SELF, ItemUsageType.PERM, true, 15)) }, 4000, "Formal Wear", "So fancy.")]
+        ROBE_5,
+        [ItemAttribute(ItemType.ROBE, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 39)), (new ItemEffect(ItemEffectType.HP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 10)) }, 7000, "Blessed Robe", "I heard its possessed by holy ghosts.")]
+        ROBE_6,
+        [ItemAttribute(ItemType.ROBE, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 42)) }, 8900, "Dragon Robe", "I can't believe a dragon could fit this.")]
+        ROBE_7,
+        [ItemAttribute(ItemType.ROBE, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 39)), (new ItemEffect(ItemEffectType.MP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 10)) }, 9000, "Magical Cassock", "Only the holiest of men can wear something so good.")]
+        ROBE_8,
+        [ItemAttribute(ItemType.ROBE, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 44)), (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 10)) }, 9900, "Ardagh Robe", "Sown by the most flamboyant dragon ever.")]
+        ROBE_9,
+        [ItemAttribute(ItemType.ROBE, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 46)), (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 15)) }, 11400, "Aeolian Robe", "Fabric so light, it seems like air.")]
+        ROBE_10,
+        [ItemAttribute(ItemType.ROBE, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 43)), (new ItemEffect(ItemEffectType.HP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 10)) }, 13500, "Oracle's Robe", "You'll feel insightful in this robe.")]
+        ROBE_11,
+        [ItemAttribute(ItemType.ROBE, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 45)), (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 20)), (new ItemEffect(ItemEffectType.AGL, ItemTargetType.SELF, ItemUsageType.PERM, true, 30)) }, 16000, "Feathered Robe", "Become as agile as a bird.")]
+        ROBE_12,
+        [ItemAttribute(ItemType.ROBE, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 47)), (new ItemEffect(ItemEffectType.MP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 20)) }, 20000, "Iris Robe", "Almost as beautiful as Iris herself.")]
+        ROBE_13,
+        [ItemAttribute(ItemType.ROBE, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 48)), (new ItemEffect(ItemEffectType.MP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 30)), (new ItemEffect(ItemEffectType.HP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 30)) }, 36000, "Mysterious Robe", "No one knows where is came from.")]
+        ROBE_14,
         #endregion 
 
         #region ARMOR
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        ARMOR_1,
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        ARMOR_2,
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        ARMOR_3,
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        ARMOR_4,
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        ARMOR_5,
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        ARMOR_6,
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        ARMOR_7,
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        ARMOR_8,
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        ARMOR_9,
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        ARMOR_10,
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        ARMOR_11,
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        ARMOR_12,
+
         #endregion
+
+        #region MELEE WEAPON
+        #endregion
+
+        #region MAGIC WEAPON
+        #endregion
+
+
     }
 }
