fbe9edb126a958771c6546ded1954670ea0426e6
Michael Sawaya <mike_gogeta@hotmail.com>
2012-04-23 17:37:14 -0700

    Updated Item List, implementing player list

--- RPG/AStarGame/AStarGame/Item.cs
+++ RPG/AStarGame/AStarGame/Item.cs
@@ -20,7 +20,7 @@ using Microsoft.Xna.Framework.Media;
 
 namespace AStarGame
 {
-    public enum ItemType {ARMOR, ROBE, CLOTHING, MELEE_WEAPON, MAGIC_WEAPON, RECOVERY_POTION, STAT_POTION, QUEST};
+    public enum ItemType {ARMOR, ROBE, CLOTHING, SWORD, MACE, STAFF, RECOVERY_POTION, STAT_POTION, QUEST};
     public enum ItemEffectType {AGL, ATK, DEF, MAG_ATK, HP, MP}
     public enum ItemTargetType { SELF, SINGLE, ALL }
     public enum ItemUsageType { TEMP, PERM, REGEN }
@@ -77,7 +77,7 @@ namespace AStarGame
         {
             return GetAttribute(i).cost;
         }
-        public static ItemEffect[] GetEffect(this Item i)
+        public static ItemEffect[] GetEffects(this Item i)
         {
             return GetAttribute(i).effect;
         }
@@ -100,7 +100,10 @@ namespace AStarGame
     public enum Item
     {
         #region QUEST
-
+        [ItemAttribute(ItemType.QUEST, new ItemEffect[] { }, 0, "Dungeon Key", "Key used to open Dragon's Cave.")]
+        DUNGEON_KEY,
+        [ItemAttribute(ItemType.QUEST, new ItemEffect[] { }, 0, "Dragon's Skull", "Skull of the Dragon King.")]
+        DRAGON_SKULL,
         #endregion
 
         #region RECOVERY POTION
@@ -182,37 +185,93 @@ namespace AStarGame
         #region ARMOR
         [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
         ARMOR_1,
-        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 21)), (new ItemEffect(ItemEffectType.MP, ItemTargetType.SELF, ItemUsageType.PERM, true, 20)) }, 1000, "Magic Armor", "Something magical about that old armor.")]
         ARMOR_2,
-        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 25)) }, 2000, "Chain Mail", "Is this really iron, it seems more like paper clips.")]
         ARMOR_3,
-        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 30)) }, 3600, "Armored Shell", "You can hide in it like a turtle.")]
         ARMOR_4,
-        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 33)) }, 4400, "Plate Mail", "You can't eat off this kind of plate.")]
         ARMOR_5,
-        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 36)) }, 4900, "Steel Armor", "Just try to move in this armor.")]
         ARMOR_6,
-        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 43)), (new ItemEffect(ItemEffectType.HP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 10)) }, 9000, "Erebus Armor", "For something so dark, it pretty light.")]
         ARMOR_7,
-        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 45)) }, 10000, "Dragon Armor", "No dragons were harmed in the making of this armor.")]
         ARMOR_8,
-        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 47)), (new ItemEffect(ItemEffectType.HP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 15)) }, 13000, "Chronos Armor", "Only time can tell if this armor is any good.")]
         ARMOR_9,
-        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 42)), (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 20)) }, 15000, "Spiked Armor", "Good thing the spikes are on the outside.")]
         ARMOR_10,
-        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 42)), (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 20)), (new ItemEffect(ItemEffectType.HP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 10)) }, 17000, "Asura's Armor", "Find balance within yourself.")]
         ARMOR_11,
-        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)) }, 240, "Leather Armor", "Smells worse than you think.")]
+        [ItemAttribute(ItemType.ARMOR, new ItemEffect[] { (new ItemEffect(ItemEffectType.DEF, ItemTargetType.SELF, ItemUsageType.PERM, true, 52)), (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 30)) }, 22000, "Lion's Armor", "Become the king of beasts.")]
         ARMOR_12,
 
         #endregion
 
-        #region MELEE WEAPON
+        #region SWORDS
+        [ItemAttribute(ItemType.SWORD, new ItemEffect[] { (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 6))}, 30, "Machete", "Good for weeding.")]
+        SWORD_1,
+        [ItemAttribute(ItemType.SWORD, new ItemEffect[] { (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 8)) }, 120, "Short Sword", "Commonly mistaken for a large knife.")]
+        SWORD_2,
+        [ItemAttribute(ItemType.SWORD, new ItemEffect[] { (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 14)) }, 500, "Long Sword", "May be misinterpreted as compensation for something.")]
+        SWORD_3,
+        [ItemAttribute(ItemType.SWORD, new ItemEffect[] { (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 20)) }, 1600, "Hunter's Sword", "Used for more than hunting game.")]
+        SWORD_4,
+        [ItemAttribute(ItemType.SWORD, new ItemEffect[] { (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 40)) }, 3500, "Broad Sword", "Can cut down small trees in one swoop.")]
+        SWORD_5,
+        [ItemAttribute(ItemType.SWORD, new ItemEffect[] { (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 55)) }, 6000, "Elven Rapier", "Only elves can produce something of this much detail.")]
+        SWORD_6,
+        [ItemAttribute(ItemType.SWORD, new ItemEffect[] { (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 62)) }, 7500, "Battle Rapier", "Always be battle ready.")]
+        SWORD_7,
+        [ItemAttribute(ItemType.SWORD, new ItemEffect[] { (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 70)) }, 9000, "Claymore", "The sword only a giant can love.")]
+        SWORD_8,
+        [ItemAttribute(ItemType.SWORD, new ItemEffect[] { (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 90)) }, 12000, "Great Sword", "Whats so great about it, its oversided and weights a ton.")]
+        SWORD_9,
+        [ItemAttribute(ItemType.SWORD, new ItemEffect[] { (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 100)), (new ItemEffect(ItemEffectType.HP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 10)) }, 15000, "Soul Blade", "It feels like my soul is attached to this sword.")]
+        SWORD_10,
         #endregion
 
-        #region MAGIC WEAPON
+        #region MACE
+        [ItemAttribute(ItemType.MACE, new ItemEffect[] { (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 4)), (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 2)) }, 80, "Mace", "A ball of metal on a stick, what else do you need.")]
+        MACE_1,
+        [ItemAttribute(ItemType.MACE, new ItemEffect[] { (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 9)), (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 5)) }, 500, "Spiked Mace", "Great for tendering meat, as well as your enemies.")]
+        MACE_2,
+        [ItemAttribute(ItemType.MACE, new ItemEffect[] { (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 18)), (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 8)) }, 2000, "Heavy Mace", "Not for the weak of arm.")]
+        MACE_3,
+        [ItemAttribute(ItemType.MACE, new ItemEffect[] { (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 28)), (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 15)) }, 4000, "Battle Mace", "Just make sure to only hit your enemies.")]
+        MACE_4,
+        [ItemAttribute(ItemType.MACE, new ItemEffect[] { (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 40)), (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 22)) }, 6500, "War Mace", "Only war would force someone to make something so dangerous.")]
+        MACE_5,
+        [ItemAttribute(ItemType.MACE, new ItemEffect[] { (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 45)), (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 27)) }, 8200, "Bladed Mace", "All the fun of beating your enemy senseless with the presision of a sword.")]
+        MACE_6,
+        [ItemAttribute(ItemType.MACE, new ItemEffect[] { (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 59)), (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 35)), (new ItemEffect(ItemEffectType.MP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 15)) }, 11000, "Blessed Mace", "When god support using a weapon, you can't say no.")]
+        MACE_7,
+        [ItemAttribute(ItemType.MACE, new ItemEffect[] { (new ItemEffect(ItemEffectType.ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 70)), (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 45)), (new ItemEffect(ItemEffectType.HP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 20)) }, 14000, "Righteous Mace", "Using this mace just feels so right.")]
+        MACE_8,
         #endregion
 
+        #region STAFF
+        [ItemAttribute(ItemType.STAFF, new ItemEffect[] { (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 6)), (new ItemEffect(ItemEffectType.MP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 3)) }, 50, "Wooden Staff", "More useful as a walking stick...")]
+        STAFF_1,
+        [ItemAttribute(ItemType.STAFF, new ItemEffect[] { (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 12)), (new ItemEffect(ItemEffectType.MP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 5)) }, 250, "Magic Staff", "Where is the compartment where the magic flowers come out?")]
+        STAFF_2,
+        [ItemAttribute(ItemType.STAFF, new ItemEffect[] { (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 20)), (new ItemEffect(ItemEffectType.MP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 8)) }, 1000, "Blessed Ankh", "Blessed by the Egyptian Gods")]
+        STAFF_3,
+        [ItemAttribute(ItemType.STAFF, new ItemEffect[] { (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 40)), (new ItemEffect(ItemEffectType.MP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 10)), (new ItemEffect(ItemEffectType.HP, ItemTargetType.SELF, ItemUsageType.REGEN, true, -10)) }, 2000, "Soul Wand", "Converts health to damage.")]
+        STAFF_4,
+        [ItemAttribute(ItemType.STAFF, new ItemEffect[] { (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 30)), (new ItemEffect(ItemEffectType.MP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 10)), (new ItemEffect(ItemEffectType.HP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 10)) }, 3500, "Regen Staff", "Holding this staff make you feel rejuvenated.")]
+        STAFF_5,
+        [ItemAttribute(ItemType.STAFF, new ItemEffect[] { (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 55)), (new ItemEffect(ItemEffectType.MP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 15)) }, 6000, "Conjurer's Staff", "Helps focus your spells.")]
+        STAFF_6,
+        [ItemAttribute(ItemType.STAFF, new ItemEffect[] { (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 68)), (new ItemEffect(ItemEffectType.MP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 15)) }, 7500, "Zodiac Sign Wand", "I asked god for a sign, he gave me Cancer.")]
+        STAFF_7,
+        [ItemAttribute(ItemType.STAFF, new ItemEffect[] { (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 75)), (new ItemEffect(ItemEffectType.MP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 18)) }, 9500, "Runic Staff", "A staff with a mysterious message written on it.")]
+        STAFF_8,
+        [ItemAttribute(ItemType.STAFF, new ItemEffect[] { (new ItemEffect(ItemEffectType.MAG_ATK, ItemTargetType.SELF, ItemUsageType.PERM, true, 90)), (new ItemEffect(ItemEffectType.MP, ItemTargetType.SELF, ItemUsageType.REGEN, true, 20)) }, 9500, "Zeus' Gold Staff", "Zeus is said to of made the color with his lightning.")]
+        STAFF_9,
+        #endregion
 
     }
 }
